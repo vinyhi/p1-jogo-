d5ec45445021cdac0643ba185000cbd77b690099
vinicius bosso <vinih_bosso@hotmail.com>
2023-10-01 16:02:50 -0300

    p1(jogo) novo personagem

--- Jogo.java
+++ Jogo.java
@@ -2,10 +2,12 @@ import java.util.Random;
 public class Jogo {
   public static void main(String[] args) throws Exception {
     Random gerador = new Random();
+    Random gerador2 = new Random();
     Personagem personagem = new Personagem("John");
+    Personagem personagem2 = new Personagem("Dio", 5, 8, 0);
     
     while(true){
-      int oQueFazer = gerador.nextInt(0); //[0..3[
+      int oQueFazer = gerador.nextInt(3); //[0..3[
       switch(oQueFazer){
         case 0:
           personagem.cacar();
@@ -19,9 +21,28 @@ public class Jogo {
       }
       personagem.morrer();
       personagem.exibirEstado();
+    
       System.out.println("--------------------");
+     
       
-      Thread.sleep(8000);
+      int oQueFazer2 = gerador2.nextInt(3); //[0..3[
+      switch(oQueFazer2){
+        case 0:
+          personagem2.cacar();
+          break;
+        case 1:
+        personagem2.comer();
+          break;
+        case 2:
+        personagem2.dormir();
+        break;
+      }
+      personagem2.morrer();
+      personagem2.exibirEstado();
+      
+      Thread.sleep(800);
+      
     }
-  }
-}
+}}
+
+
